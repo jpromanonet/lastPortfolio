d5960ba4becb66a3c708a19e79f7fe354eae88ad
jpromanonet <programming@jpromano.net>
2023-01-13 12:00:52 -0300

    Navbar done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import NavBar from './components/NavBar'
 import Grid from './components/Grid'
 import Footer from './components/Footer'
 import './App.css';
-//changes to imports 
-import SecurityIcon from '@material-ui/icons/Security';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import TrendingUpIcon from '@material-ui/icons/TrendingUp';
-import ImportExportIcon from '@material-ui/icons/ImportExport';
-import ComputerIcon from '@material-ui/icons/Computer';
-import HttpIcon from '@material-ui/icons/Http';
 
 const theme = createMuiTheme({
   palette: {
@@ -46,9 +39,6 @@ const styles = makeStyles({
   bigSpace: {
     marginTop: "5rem"
   },
-  littleSpace:{
-    marginTop: "2.5rem",
-  },
   grid:{
     display: "flex", 
     justifyContent: "center",
@@ -73,14 +63,11 @@ function App() {
           </Typography>
         </div>
         <div className={`${classes.grid} ${classes.bigSpace}`}>
-          <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
-          <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
-          <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
+          <a href="https://jpromano.net" target="_blank" >
+            <Grid title="Expanding Cards" btnTitle="Show me More"/>
+          </a>
         </div>
         <div className={`${classes.grid} ${classes.littleSpace}`}>  
-          <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
-          <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
-          <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
         </div>
         <div className={classes.bigSpace}>
           <Footer/>
