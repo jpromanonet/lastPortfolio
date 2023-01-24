5a98404148b64ad0d3984d7aaa59ba701aa68bca
jpromanonet <programming@jpromano.net>
2023-01-24 16:47:41 -0300

    New project added

--- src/App.js
+++ src/App.js
@@ -316,6 +316,24 @@ function App() {  const classes = styles();
           </CardActions>
         </Card>
 
+        <Card style={{margin: "20px", width: "300px"}}>
+          <CardHeader
+            title="FAQ Collapse"
+          />
+          <CardMedia
+          >
+            <img src={require('./assets/faq.png')} width="100%" height="200px"/>
+          </CardMedia>
+          <CardActions style={{justifyContent: "center"}}>
+            <a href="https://faq-collapse-rose.vercel.app/" target="_blank" >
+              <Button>Live</Button>
+            </a>
+            <a href="https://github.com/jpromanonet/faqCollapse" target="_blank">
+              <Button>Github</Button>
+            </a>
+          </CardActions>
+        </Card>
+
         </div>
 
         <div className={classes.bigSpace}>
