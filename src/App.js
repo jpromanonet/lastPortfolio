239954907f63a6b64272a6e6282ec7fefa7571c8
jpromanonet <programming@jpromano.net>
2023-02-14 18:00:05 -0300

    Project added

--- src/App.js
+++ src/App.js
@@ -522,6 +522,28 @@ function App() {  const classes = styles();
 
         </div>
 
+        <div className={`${classes.grid} ${classes.bigSpace}`}>
+
+        <Card style={{margin: "20px", width: "300px"}}>
+          <CardHeader
+            title="Simple Drawing App"
+          />
+          <CardMedia
+          >
+            <img src={require('./assets/bgslider.png')} width="100%" height="200px"/>
+          </CardMedia>
+          <CardActions style={{justifyContent: "center"}}>
+            <a href="https://backgroundslider.vercel.app/" target="_blank" >
+              <Button>Live</Button>
+            </a>
+            <a href="https://github.com/jpromanonet/backgroundSlider" target="_blank">
+              <Button>Github</Button>
+            </a>
+          </CardActions>
+        </Card>
+
+        </div>
+
         <div className={classes.bigSpace}>
           <Footer/>
         </div>
