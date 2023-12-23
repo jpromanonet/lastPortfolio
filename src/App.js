a85ee57555daa9434b922f198c3ada9a9a1de794
jpromanonet <programming@jpromano.net>
2023-12-23 14:51:07 -0300

    Done by now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import NavBar from "./components/NavBar";
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Footer from "./components/Footer";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
@@ -9,18 +9,49 @@ import CardActions from "@material-ui/core/CardActions";
 import { CardHeader, CardMedia } from "@material-ui/core";
 import projectsData from "./projects.json";
 import { useStyles, theme } from "./styles";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
 import "./App.css";
 
 function App() {
   const classes = useStyles();
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const appliedTheme = createTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main:darkMode ? "#ffffff" : theme.palette.primary.main // Maintain the primary color
+      },
+      background: {
+        default: darkMode ? "#121212" : "#ffffff",
+        paper: darkMode ? "#1E1E1E" : "#ffffff",
+      },
+      text: {
+        primary: darkMode ? "#ffffff" : "#000000",
+      },
+    },
+  });
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <NavBar />
+      <ThemeProvider theme={appliedTheme}>
+        <CssBaseline />
+        <br/>
+        <div className={classes.navBar}>
+          {/* Dark Mode Toggle Button */}
+          <Button color="primary" onClick={toggleDarkMode} className={classes.darkModeToggle}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </Button>
+        </div>
+
         <div className={classes.wrapper}>
           <Typography variant="h4" className={classes.bigSpace} color="primary">
-          Hi! I'm Juan and this is my portfolio
+            Hi! I'm Juan and this is my portfolio
             <br />
             <br />
           </Typography>
@@ -29,9 +60,10 @@ function App() {
             className={classes.littleSpace}
             color="primary"
           >
-          👨‍💼 Engineering Manager | 👨‍💻 Full Stack Developer | 👨‍🏫 Teacher | ✍️ Writer
+            👨‍💼 Ethical Hacker | 👨‍💻 Indie Hacker | 👨‍🏫 Teacher | ✍️ Writer
           </Typography>
         </div>
+
         {/* Project Cards */}
         <div className={`${classes.grid} ${classes.bigSpace}`} style={{ alignItems: "center" }}>
           {projectsData.map((project, index) => (
